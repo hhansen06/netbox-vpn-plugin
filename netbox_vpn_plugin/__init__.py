55a4784c0be2710a1e784667292bbeb44285e47d
Henrik Hansen <henrik.hansen@cgi.com>
2022-06-23 10:38:06 +0200

    release 0.4

--- netbox_vpn_plugin/__init__.py
+++ netbox_vpn_plugin/__init__.py
@@ -2,11 +2,11 @@ from extras.plugins import PluginConfig
 
 class NetBoxVpnConfig(PluginConfig):
     name = 'netbox_vpn_plugin'
-    verbose_name = ' NetBox VPN Plugin'
+    verbose_name = 'VPN'
     author = "Henrik Hansen"
     author_email = "henrik.hansen@cgi.com"
     description = 'Manage VPN Configurations for Tenants'
-    version = '0.3'
+    version = '0.4'
     base_url = 'vpn'
     
 config = NetBoxVpnConfig
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='netbox-vpn-plugin',
-    version='0.3.1',
+    version='0.4',
     description='Manage VPN Connections in Netbox',
     install_requires=[],
     packages=find_packages(),
