0119bd44757e53782c365772267bea5393ae2496
Henrik Hansen <henrik.hansen@cgi.com>
2022-06-23 10:39:46 +0200

    updated url

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='netbox-vpn-plugin',
     version='0.4',
+    download_url='https://github.com/hhansen06/netbox-vpn-plugin/archive/refs/tags/0.4.tar.gz'
     description='Manage VPN Connections in Netbox',
     install_requires=[],
     packages=find_packages(),
